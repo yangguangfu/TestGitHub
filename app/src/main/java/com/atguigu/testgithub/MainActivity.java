d66d5f938e028ec81d6ddb7585974c5055750778
atguiguafu <541433511@qq.com>
2016-08-12 00:03:45 +0800

    新内容

--- app/src/main/java/com/atguigu/testgithub/MainActivity.java
+++ app/src/main/java/com/atguigu/testgithub/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //我是阿福
+        //我是阿福 
+        
+        //添加新功能哦
     }
 }
